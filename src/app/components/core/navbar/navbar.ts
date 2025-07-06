110756ba50c5e84c3087af0263235fca8a35da05
ArelyEli <65979756+ArelyEli@users.noreply.github.com>
2025-07-06 10:33:04 -0700

    navbar added

---
+++ src/app/components/core/navbar/navbar.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/auth';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar implements OnInit {
+  auth = inject(AuthService);
+  router = inject(Router);
+  isAuthenticated = this.auth.isAuthenticated;
+
+  ngOnInit() {
+    this.auth.checkSession();
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
